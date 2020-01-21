ef3a0a6556662683234bd81fe872865efa32b82b
runningiris <irisrunning@foxmail.com>
2020-01-21 15:07:18 +0800

    update 买卖股票的最佳时机

--- algorithms/primary/dynamic-programming/max-profit.cpp
+++ algorithms/primary/dynamic-programming/max-profit.cpp
@@ -7,6 +7,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int size = (int)prices.size();
+        if (size < 1) return 0;
         int min = prices[0];
         int maxProfit = 0;
         for (int i = 1; i < size; i++) {
