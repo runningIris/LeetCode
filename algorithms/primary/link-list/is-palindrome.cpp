064de209d53b3733ac1a4fecc6eb9b22bd4cd017
runningiris <irisrunning@foxmail.com>
2020-02-04 21:16:30 +0800

    回文链表：栈方法

--- algorithms/primary/link-list/is-palindrome.cpp
+++ algorithms/primary/link-list/is-palindrome.cpp
@@ -1,1 +1,24 @@
 // 回文链表
+
+// 栈方法对比
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head == NULL) return 1;
+        stack<int> container;
+        int count = 0;
+        ListNode* current = head;
+        while(current!=NULL) {
+            container.push(current->val);
+            count++;
+            current = current->next;
+        }
+
+        for(int i = 0; i < count/2; i++) {
+            if (container.top() != head->val) return 0;
+            container.pop();
+            head = head->next;
+        }
+        return 1;
+    }
+};
